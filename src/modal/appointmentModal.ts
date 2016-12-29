16519c9691e1dfd9b0659556c0a2220ebe776c8e
mjkhan25 <jamal.brayan@gmail.com>
2016-12-29 17:06:26 +0530

    chnages modal

--- src/modal/appointmentModal.ts
+++ src/modal/appointmentModal.ts
@@ -3,12 +3,12 @@ export class Appointment
   public userId:number;
   public id:number;
   public imgPath:String;
-   public name:String;
-    public designation:String;
-     public dateOne:String;
-      public department:String;
-       public location:String;
-        public dateTwo:String;
-         public appointmentTime:String;
+  public name:String;
+  public title:String;
+  public appointmentDateTime:String;
+  public department:String;
+  public location:String;
+  public phone:String;
+         
 
 }
