64fa17dcd012c5cc7f024b94c7e353f1e8b67fc9
mjkhan25 <jamal.brayan@gmail.com>
2017-01-16 18:39:13 +0530

    appointment prep ui

--- src/pages/appointmentPrepComponent/appointmentPrepComponent.ts
+++ src/pages/appointmentPrepComponent/appointmentPrepComponent.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
-
+declare var jQuery: any;
 
 
 @Component({
@@ -16,6 +16,19 @@ export class appointmentPrepComponent {
   }
 
 
+
+  // code for card toggle 
+  collapse(event) {
+    var _current = (event.target) ? event.target : event;
+    if(jQuery(_current).hasClass('cardClickDown')) {
+      jQuery(_current).removeClass('cardClickDown');
+    } else {
+      jQuery('.prepAddQuestion .cardClickDown').removeClass('cardClickDown');
+      jQuery('.prepAddQuestion .wrapBox').slideUp();
+      jQuery(_current).addClass('cardClickDown');
+    }
+    jQuery(_current).next('.wrapBox').slideToggle();
+  }
 
 }
 
